0e529fa5b0c3dcd558b9871e0763e82f70428db0
Nightshade23x <samarmahajan23@gmail.com>
2026-01-12 09:30:53 +0200

    installed django and began the project by doing migration etc

---
+++ notes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    # Used later to demonstrate Broken Access Control
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
