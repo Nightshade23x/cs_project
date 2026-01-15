7613ca54c3f3d9e8bd3aa11f75ef4a5d50c98170
Nightshade23x <samarmahajan23@gmail.com>
2026-01-15 09:38:43 +0200

    worked on flaw 2 which is sql injection

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("note/<int:note_id>/", views.view_note, name="view_note"),
+    path("search/", views.search_notes, name="search_notes"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db import connection
 from .models import Note
 
 
@@ -9,4 +10,21 @@ def view_note(request, note_id):
 
     # FIX:
     # note = Note.objects.get(id=note_id, owner=request.user)
-  
+
+def search_notes(request):
+    query = request.GET.get("q", "")
+    user_id = request.user.id
+
+    #FLAW 2: SQL Injection
+    sql = f"""
+        SELECT * FROM notes_note
+        WHERE owner_id = {user_id}
+        AND title LIKE '%{query}%'
+    """
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+    return render(request, "search_results.html", {"rows": rows, "query": query})
+
+    # FIX:
+    # notes = Note.objects.filter(owner=request.user, title__icontains=query)
